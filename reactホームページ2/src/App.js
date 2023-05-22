c988d2c67896cd803906f2f6c7cd225bdbf99ea9
bayashiwaka <134250407+bayashiwaka@users.noreply.github.com>
2023-05-22 12:43:52 +0900

    Add files via upload

---
+++ reactホームページ2/src/App.js
@@ -0,0 +1,33 @@
+import { BrowserRouter } from "react-router-dom";
+import { PrimaryButton } from "./components/atoms/button/PrimaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInput";
+import { UserCard } from "./components/organisms/user/UserCard";
+import { DefaultLayout } from "./components/templates/DefaultLayout";
+import { HeaderOnly } from "./components/templates/HeaderOnly";
+import "./styles.css";
+
+const user = {
+  name: "わかさま",
+  image: "https://source.unsplash.com/NE0XGVKTmcA",
+  email: "12345example.com",
+  phone: "090-828-828",
+  company: {
+    name: "テスト株式会社"
+  },
+  website: "https://google.com"
+};
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <DefaultLayout>
+        <PrimaryButton>テスト</PrimaryButton>
+        <SecondaryButton>検索</SecondaryButton>
+        <br />
+        <SearchInput />
+        <UserCard user={user} />
+      </DefaultLayout>
+    </BrowserRouter>
+  );
+}
